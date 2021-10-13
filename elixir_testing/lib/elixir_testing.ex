27ab480ce8765185cc3a91c7f6c7174bce51b39f
David Gayman <davidrgayman@gmail.com>
2021-10-13 09:43:57 -0400

    Cleaning up tests.

--- elixir_testing/lib/elixir_testing.ex
+++ elixir_testing/lib/elixir_testing.ex
@@ -10,30 +10,10 @@ defmodule ElixirTesting do
   end
 
   # ----------------------------------------------------------------------------
-  # region Global Constant
-  # ----------------------------------------------------------------------------
-
-  @horizontal_line String.duplicate("-", 80)
-
-  def test_gobal_constant do
-    IO.puts(@horizontal_line)
-    IO.puts("Console Report Section Title")
-    IO.puts(@horizontal_line)
-    true
-  end
-
-  # ----------------------------------------------------------------------------
   # region Static Function Variable
   # ----------------------------------------------------------------------------
 
-  def test_static_function_variable do
-    IO.puts(get_value_from_db_cache("A"))
-    IO.puts(get_value_from_db_cache("B"))
-    IO.puts(get_value_from_db_cache("A"))
-    true
-  end
-
-  defp get_value_from_db_cache(key) do
+  def get_value_from_db_cache(key) do
 
     # We want this to be static.
     data_cache = %{}
@@ -47,7 +27,7 @@ defmodule ElixirTesting do
   end
 
   @db_data %{ "A" => 3, "B" => 4, "C" => 5, "D" => 6 }
-  defp get_value_directly_from_db(key) do
+  def get_value_directly_from_db(key) do
     IO.puts("Inside get_value_directly_from_db for key #{key}")
     @db_data[key]
   end
@@ -58,41 +38,6 @@ defmodule ElixirTesting do
 
   defmodule IceCreamFlavorEnumeration do
     defstruct vanilla: 0, chocolate: 1, peanut_butter: 2
-  end
-
-  def test_enumeration do
-
-    e = %IceCreamFlavorEnumeration{}
-
-    v = e.vanilla
-    c = e.chocolate
-    p = e.peanut_butter
-
-    assert v < c
-    assert c < p
-
-  end
-
-  # ----------------------------------------------------------------------------
-  # region Macro Testing
-  # ----------------------------------------------------------------------------
-
-  def test_macro_unless do
-    require Macros
-    Macros.fun_unless(true, do: IO.puts "this should never be printed 1")
-    Macros.macro_unless true, do: IO.puts "this should never be printed 2"
-    :ok
-  end
-
-  def test_macro_ternary do
-    import Macros
-#    IO.puts Macros.ternary(true, "True expression.", "False expression.")
-    IO.puts if?(true, "True expression.", "False expression.")
-    IO.puts if?(false, "True expression.", "False expression.")
-#    IO.puts true ||| "True expression.", "False expression."
-#    IO.puts if3?(true, do: "True expression.")
-#    IO.puts if3?(false, do: "True expression.")
-    :ok
   end
 
   # ----------------------------------------------------------------------------
@@ -121,26 +66,40 @@ defmodule ElixirTesting do
 
   defp internal_fcn(stream), do: Stream.map(stream, &(&1 * 2))
 
+  # ----------------------------------------------------------------------------
+  # region Warpath (https://hexdox.pm/warpath/Warpath.html) Testing
+  # ----------------------------------------------------------------------------
+
+
+
+
+  # Decomposition
+  Warpath.query!(m, "$..*")
+
+  # Sorting
+
+  # Composition
+
+
+
+  m = %{
+      a: %{
+        "b" => [
+          %{
+            "c" => [
+              "d",
+              "e"
+            ]
+          }
+        ]
+      }
+    }
+
+    q = Warpath.query!(m, "$.a")
+    IO.inspect q
+    assert  == q
+
+  end
+
+
 end
-
-
-
-
-
-#defmodule Macros do
-
-#end
-
-
-#
-#      t = {:a, :b, :c}
-#
-#
-#      IO.inspect t~>0
-#      IO.inspect t~>1
-#      i=2
-#      IO.inspect t~>i
-#
-#      IO.inspect if true ||| :a | :b
-#      IO.inspect if false ||| :a | :b
-
--- elixir_testing/test/elixir_testing_test.exs
+++ elixir_testing/test/elixir_testing_test.exs
@@ -1,24 +1,62 @@
 defmodule ElixirTestingTest do
   use ExUnit.Case
 
+  @horizontal_line String.duplicate("-", 80)
   test "test_gobal_constant" do
-    assert ElixirTesting.test_gobal_constant
+    IO.puts(@horizontal_line)
+    IO.puts("Console Report Section Title")
+    IO.puts(@horizontal_line)
+    assert true
   end
 
   test "test_static_function_variable" do
-    assert ElixirTesting.test_static_function_variable
+    IO.puts(ElixirTesting.get_value_from_db_cache("A"))
+    IO.puts(ElixirTesting.get_value_from_db_cache("B"))
+    IO.puts(ElixirTesting.get_value_from_db_cache("A"))
+    assert true
   end
 
   test "test_enumeration" do
-    assert ElixirTesting.test_enumeration
+    e = %ElixirTesting.IceCreamFlavorEnumeration{}
+
+    v = e.vanilla
+    c = e.chocolate
+    p = e.peanut_butter
+
+    assert v < c
+    assert c < p
   end
 
   test "test_macro_unless" do
-    assert ElixirTesting.test_macro_unless
+    require Macros
+    Macros.fun_unless(true, do: IO.puts "this should never be printed 1")
+    Macros.macro_unless true, do: IO.puts "this should never be printed 2"
+    assert true
   end
 
   test "test_macro_ternary" do
-    assert ElixirTesting.test_macro_ternary
+    import Macros
+    #    IO.puts Macros.ternary(true, "True expression.", "False expression.")
+    IO.puts if?(true, "True expression.", "False expression.")
+    IO.puts if?(false, "True expression.", "False expression.")
+    #    IO.puts true ||| "True expression.", "False expression."
+    #    IO.puts if3?(true, do: "True expression.")
+    #    IO.puts if3?(false, do: "True expression.")
+
+    #      IO.inspect if true ||| :a | :b
+    #      IO.inspect if false ||| :a | :b
+    assert true
+  end
+
+  test "test_macro_tuple_element" do
+    #      t = {:a, :b, :c}
+    #
+    #
+    #      IO.inspect t~>0
+    #      IO.inspect t~>1
+    #      i=2
+    #      IO.inspect t~>i
+    assert false
   end
 
 #  test "test_functionalized_stream_composition" do
@@ -31,5 +69,12 @@ defmodule ElixirTestingTest do
 #
 #    assert ret_1 == ret_2
 #  end
+
+  test "warpath_testing1" do
+    document = %{"elements" => [:a, :b, :c]}
+    q = Warpath.query!(document, "$.elements[*]")
+    IO.inspect q
+    assert [:a, :b, :c] == q
+  end
 
 end
